821fe95500577e11dcae1878a16e2619790fc8c8
Leeseunghwan <sun1238799@naver.com>
2021-08-15 01:35:27 +0900

    CallBack Study

--- arc/call.js
+++ arc/call.js
@@ -16,3 +16,28 @@ setTimeout(() => console.log("2"), 1000);
 console.log("3");
 printImmediately(() => console.log("hello"));
 printWithDelay(() => console.log("async callback"), 2000);
+
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
+        (id === "ellie" && password === "dream") ||
+        (id === "coder" && password === "academy")
+      ) {
+        onSuccess(id);
+      } else {
+        onError(new Error("not found"));
+      }
+    }, 2000);
+  }
+
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === "ellie") {
+        onSuccess({ name: "ellie", role: "admin" });
+      } else {
+        onError(new Error("no access"));
+      }
+    }, 1000);
+  }
+}

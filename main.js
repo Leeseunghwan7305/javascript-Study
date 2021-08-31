7edd2e06772985da4142b1b1e53966704d7f8086
Leeseunghwan <sun1238799@naver.com>
2021-09-01 07:48:25 +0900

    tap-event

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-function pre() {}
-
-$(".slide-2").click(function () {
-  $(".slide-container").css("transform", "translateX(-100vw)");
-});
-$(".slide-3").click(function () {
-  $(".slide-container").css("transform", "translateX(-200vw)");
-});
-$(".slide-1").click(function () {
-  $(".slide-container").css("transform", "translateX(0)");
-});
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 50) {
-    $(".nav-menu").addClass("nav-black");
-  }
-});
+$(".tab-button")
+  .eq(0)
+  .click(function () {
+    $(".tab-button").removeClass("active");
+    $(".tab-content").removeClass("show");
+    $(".tab-button").eq(0).addClass("active");
+    $(".tab-content").eq(0).addClass("show");
+  });
+$(".tab-button")
+  .eq(1)
+  .click(function () {
+    $(".tab-button").removeClass("active");
+    $(".tab-content").removeClass("show");
+    $(".tab-button").eq(1).addClass("active");
+    $(".tab-content").eq(1).addClass("show");
+  });
+$(".tab-button")
+  .eq(2)
+  .click(function () {
+    $(".tab-button").removeClass("active");
+    $(".tab-content").removeClass("show");
+    $(".tab-button").eq(2).addClass("active");
+    $(".tab-content").eq(2).addClass("show");
+  });

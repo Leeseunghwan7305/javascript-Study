7a0c0781851fd52a4b0e53d819104ae3f2f56558
Leeseunghwan <sun1238799@naver.com>
2021-09-03 12:48:31 +0900

    ajax

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-$(".tab-button")
-  .eq(0)
-  .click(function () {
-    $(".tab-button").removeClass("active");
-    $(".tab-content").removeClass("show");
-    $(".tab-button").eq(0).addClass("active");
-    $(".tab-content").eq(0).addClass("show");
+$(".bb").click(function () {
+  $.ajax({
+    url: `https://codingapple1.github.io/data.json `,
+    type: `GET`,
+  }).done(function (데이터) {
+    $(".card-img-top").attr("src", 데이터.img);
+    $(".card-title").html(데이터.brand);
+    $(".card-text").html(데이터.model);
+    $(".price").html(데이터.price);
+    console.log(데이터);
   });
-$(".tab-button")
-  .eq(1)
-  .click(function () {
-    $(".tab-button").removeClass("active");
-    $(".tab-content").removeClass("show");
-    $(".tab-button").eq(1).addClass("active");
-    $(".tab-content").eq(1).addClass("show");
-  });
-$(".tab-button")
-  .eq(2)
-  .click(function () {
-    $(".tab-button").removeClass("active");
-    $(".tab-content").removeClass("show");
-    $(".tab-button").eq(2).addClass("active");
-    $(".tab-content").eq(2).addClass("show");
-  });
+});
